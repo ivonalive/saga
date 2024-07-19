e17028d2b6ba1da0b1353d252d475441d20bb377
Conor Callahan <conorcallahan97@outlook.com>
2024-07-18 19:43:24 -0500

    attempted to import the hidden api key

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,16 @@
+import 'dotenv/config';
+
+const GIPHY_API_KEY =process.env.GIPHY_API_KEY
+
+
+
 function App() {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <div id="gif-container">
+    <p>{GIPHY_API_KEY}</p>
+      </div>
     </div>
   );
 }
